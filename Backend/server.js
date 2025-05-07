d61c489926c1c0f475ee606e5d2e337dfebcbcf4
Cheethan <cheethan2004@gmail.com>
2025-05-08 03:12:25 +0530

    Updated cors server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://price-comparison-site-smoky.vercel.app/', 
+  origin: 'https://price-comparison-site-smoky.vercel.app', 
   credentials: true
 }));
 
